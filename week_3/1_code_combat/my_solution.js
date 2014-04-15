2deac0a8df690408ccb2c984523092d494974b42
Spencer Olson <spence1818@gmail.com>
2014-04-15 00:08:57 -0500

    Completed CodeCombat Exercises
    Completed the Exercises. Still need to add my reflection though.

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -1,16 +1,120 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge by myself
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
+// RESCUE MISSION
+// this.moveDown(); //move down * 2
+// this.moveDown(); 
+// this.moveRight(); //move right 
+// this.moveUp();	//move up * 2
+// this.moveUp();
+// this.moveRight(); //move right * 2
+// this.moveRight();
+// this.moveDown();	//move down
+// this.attackNearbyEnemy(); //kill the Ogre!
+
+// GRAB THE MUSHROOM
+// this.moveUp(); //move up
+// this.moveRight(); //move towards the mushroom
+// this.moveLeft(); //turn around after getting the mushroom
+// this.moveUp(); //move towards the Ogre
+
+// this.attackNearbyEnemy(); //kill the Ogre!
+
+//DRINK ME
+// this.attackNearbyEnemy(); //kill first Munchkin
+// this.moveRight(); //move one space right
+// this.moveDown(); //walk towards the poition
+// this.moveUp(); //turn around after grabbing potion
+// this.moveRight(); //walk towards 2nd Munchkin
+// this.attackNearbyEnemy(); //kill 2nd Munchkin
+
+//TAUNT THE GUARDS
+// this.moveRight();
+// this.bustDownDoor();
+
+// // Delete the "//" in front of each line below.
+// this.moveRight();
+// this.say("Hey there!"); //Call first guard over
+// this.moveLeft(); //move past door to give Phoebe a clear shot
+// this.moveLeft();
+// this.say("Attack!"); /tell Phoebe to attack the first guard
+
+// // Now get Phoebe to follow through the dungeon.
+// this.moveRight();
+// this.say("Follow me."); //Tell Phoebe to follow you while you escape
+// this.moveRight();
+// this.moveRight();
+// this.moveUp();
+// this.moveRight();
+// this.say("Run!"); //Yell to get guards to come over
+// this.moveDown(); //Escape by taking the circular side path
+// this.moveRight();
+// this.moveUp();
+// this.moveRight();
  
  
+//  IT'S A TRAP
+//  // Taunt the Ogre and run for cover!
+// this.moveDown(); //move to the bottom of the screen to get the ogre's attention
+// this.moveDown();
+// this.moveDown();
+// this.moveUp(); //once the ogre sees you, head back where you came from to lure him into the archers' sight
+// this.moveUp();
+// this.moveUp();
  
- 
- 
- 
- 
- 
+//  TAUNT
+//  this.say("Hey!");
+
+// // Lure the ogre right into your arrow trap
+// // by saying a few more things at him.
+// // Anything works!
+// this.say("I spit in your general direction"); //Unleash the three most vicious insults ever
+// this.say("Ni!");
+// this.say("I'm going to take your mother out for a nice steak dinner and never call her back.");
+
+// COWARDLY TAUNT
+// this.moveXY(50, 16);
+// this.moveXY(63, 20);
+// this.moveXY(70, 10);  // This is a safe spot.
+
+// this.say("I can lure them in here.");
+
+// // Run your soldier out where the ogres can hear you.
+// this.moveXY(63,20); //move out towards the towers
+// // Say something!
+// this.say("Did someone order a pizza?"); //lure the ogres in and watch them die
+
+// COMMANDING FOLLOWER
+// this.moveXY(49, 66);
+// this.moveXY(60, 63);
+// this.moveXY(75, 63);
+// this.say("Hail, friends!");
+
+// // Saying anything with "follow" in it will get nearby soldiers
+// // to follow you
+// this.say("Don't follow me, please!"); //get them to follow you
+// // Saying anything with "attack" will send nearby troops into
+// // a battle mode.
+// this.moveXY(68,51); //move towards the ogres
+// this.say("Ow, i just stepped on attack!"); //tell the troops to attack
+// this.say("Ok attack fools");
+// // Make sure Tharin is safe!
+
+// MOBILE ARTILLERY
+// this.moveXY(30, 26);  // Move into range
+// this.attackXY(46, 5);  // Shoot once in the middle of the ogres
+
+// // If they survive a hit, ogres retaliate!
+// // Larger ogres take more than one hit...
+// // So, position and aim carefully.
+// this.moveXY(42,41); //move and attack first pack of ogres twice
+// this.attackXY(50,69);
+// this.attackXY(45,52);
+// this.attackXY(67,54); //attack second pack of ogres twice
+// this.attackXY(58,47);
+
 // Reflection:
 // Write your reflection here.
