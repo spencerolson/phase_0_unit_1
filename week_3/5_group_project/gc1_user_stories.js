31161989e958f3769f5a139f96f0fffcd194237d
Spencer Olson <spence1818@gmail.com>
2014-04-19 19:58:22 -0600

    Completed Part 1 of Group Project

---
+++ week_3/5_group_project/gc1_user_stories.js
@@ -0,0 +1,5 @@
+// I want to create a function called sum that gives me the sum of a group of numbers.
+
+// I want to create a function called mean that gives me the average of a group of numbers.
+
+// I want to create a function called median that gives me the median value of a group of numbers.
