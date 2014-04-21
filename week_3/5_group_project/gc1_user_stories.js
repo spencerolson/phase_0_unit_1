bb5d202a899993ac838be69d3838e2e5c672e7c6
Spencer Olson <spence1818@gmail.com>
2014-04-20 20:07:51 -0600

    Added Reflection for the Group Challenge

--- week_3/5_group_project/gc1_user_stories.js
+++ week_3/5_group_project/gc1_user_stories.js
@@ -3,3 +3,21 @@
 // I want to create a function called mean that gives me the average of a group of numbers.
 
 // I want to create a function called median that gives me the median value of a group of numbers.
+
+//Reflection
+
+// What parts of your strategy worked? What problems did you face?
+// What questions did you have while coding? What resources did you find to help you answer them?
+// What concepts are you having trouble with, or did you just figure something out? If so, what?
+// Did you learn any new skills or tricks?
+// How confident are you with each of the Learning Competencies?
+// Which parts of the challenge did you enjoy?
+// Which parts of the challenge did you find tedious?
+
+// To be honest, I did not have too many issues with this challenge. I was user #1, so I created the user stories for the next person to create pseudocode from. The hardest part of creating the user stories was avoiding using programmer jargon, e.g. array or method. I would say that a skill I learned was figuring out how to explain programming jargon in 'normal people' terms. I feel confident with each of the Learning Competencies.
+
+// What was this experience like?
+// Was your group successful in passing the tests?
+// What did you learn about writing user stories, pseudocoding, writing code, refactoring, etc.?
+
+// This experience was enjoyable. At the time I'm writing this reflection, I have not seen the end product, so I can't say whether or not my group was successful in passing the tests. I think the most valuable thing I learned was how to write user stories without including programmer jargon.
